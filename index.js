9c0d9b9b1698f3c65e36d6d53a3dabca659b7960
Mercede Chen <mercedechen@gmail.com>
2023-12-02 18:10:48 -0500

    Add form to add party event

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const cohort = "2310-fsa-et-web-pt-sf-b-mercede/events";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}`;
 
-
 const state = {
-  parties: [],
+  events: [],
 };
 
 const ul = document.getElementById("parties");
@@ -11,11 +10,12 @@ const ul = document.getElementById("parties");
 async function getParties() {
   try {
     const response = await fetch(API_URL);
-    const partyList = await response.json();
+    const jsonResponse = await response.json();
+    const parties = jsonResponse.data;
+    console.log(parties);
   } catch (error) {
    console.error(error);
   }
 };
 getParties();
-
 
