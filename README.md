4e0ba53b553e4c9b3551bc95151aac6407b5441d
Mercede Chen <mercedechen@gmail.com>
2023-12-03 14:53:42 -0500

    Add functionality to create party posts

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Your friend is an event organizer and has asked you to build a website for them. They want to be able to keep track of all the parties they are organizing. They want to be able to see a list of all the parties, add new parties, and delete parties.
 
-# Requirements
+## Requirements
 
 Build your website according to this user story:
 
@@ -10,4 +10,16 @@ A user enters the website and finds a list of the names, dates, times, locations
 Next to each party in the list is a delete button. The user clicks the delete button for one of the parties. That party is then removed from the list.
 
 There is also a form that allows the user to enter information about a new party that they want to schedule. After filling out the form and submitting it, the user observes their party added to the list of parties.
+
+## Criteria
+
+* Fetch is used correctly to GET party data from the API.
+* Fetch is used correctly to POST a new party to the API.
+* Fetch is used correctly to DELETE a party from the API.
+* The app contains a list of the names, dates, times, locations, and descriptions of all parties.
+* Each party in the list has a delete button which removes the party when clicked.
+* The app contains a form that allows a user to enter information about a party and add it to the list.
+* The DOM is dynamically rendered according to data stored in state.
+* The data stored in state is updated to stay in sync with the API.
+
 
--- index.js
+++ index.js
@@ -1,40 +1,77 @@
-const cohort = "2310-FSA-ET-WEB-PT-SF-mercede";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}/events`;
+const COHORT = "2310-FSA-ET-WEB-PT-SF-mercede";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
   parties: [],
 };
 
-const partyList = document.querySelector("#parties");
+const parties = document.querySelector("#parties");
+const addPartyForm = document.querySelector("#partyForm");
+addPartyForm.addEventListener("submit", addParty);
 
+// Wait to get party data first, then display on HTML
 async function render() {
   await getParties();
   renderParties();
 }
 render();
 
+// 1. Get events data from API URL
 async function getParties() {
   try {
     const response = await fetch(API_URL);
     const json = await response.json();
     const parties = json.data;
     state.parties = parties;
-    console.log("parties", parties);
-    return parties;
+    console.log(state.parties);
   } catch (error) {
-   console.error(error.message);
+   console.log(error);
   }
 };
 
+// 2. Display party events on HTML using DOM
 function renderParties() {
-  const partyCards = state.parties.map((party) => {
+  const partyPosts = state.parties.map((party) => {
     const li = document.createElement("li");
-    li.innerHTML = 
-    `<h2>${party.name}</h2>
-    <p>${party.date}</p>
+    li.innerHTML = `
+    <h2>${party.name}</h2>
+    <p>${new Date(party.date).toLocaleString("en-US")}</p>
     <p>${party.location}</p>
-    <p>${party.description}</p>`;
+    <p>${party.description}</p>
+    `;
+
+    const remove = document.createElement("button");
+    remove.textContent = "remove";
+    remove.addEventListener("click", removePost);
+    li.append(remove);
     return li;
   });
-  partyList.replaceChildren(...partyCards);
+  partyList.append(...partyPosts);
 }
+
+// 3. Add party post
+async function addParty(event) {
+  event.preventDefault();
+
+  try {
+     const res = await fetch (API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: addPartyForm.name.value,
+        description: addPartyForm.description.value,
+        date: new Date(addPartyForm.date.value).toISOString(),
+        location: addPartyForm.location.value,
+      }),
+    });
+
+    if (res.ok) {
+      render();
+    } else {
+      throw new Error("Failed to create party post");
+    }
+
+  } catch (error) {
+    console.log(error);
+  };
+};
