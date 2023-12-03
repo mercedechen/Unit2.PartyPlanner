0cf9499649d113470fb91b80fdbca88aa58193ed
Mercede Chen <mercedechen@gmail.com>
2023-12-02 20:05:02 -0500

    Add style and display party cards

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const cohort = "2310-fsa-et-web-pt-sf-b-mercede/events";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}`;
+const cohort = "2310-FSA-ET-WEB-PT-SF-mercede";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${cohort}/events`;
 
 const state = {
-  events: [],
+  parties: [],
 };
 
-const ul = document.getElementById("parties");
+const partyList = document.querySelector("#parties");
+
+async function render() {
+  await getParties();
+  renderParties();
+}
+render();
 
 async function getParties() {
   try {
     const response = await fetch(API_URL);
-    const jsonResponse = await response.json();
-    const parties = jsonResponse.data;
-    console.log(parties);
+    const json = await response.json();
+    const parties = json.data;
+    state.parties = parties;
+    console.log("parties", parties);
+    return parties;
   } catch (error) {
-   console.error(error);
+   console.error(error.message);
   }
 };
-getParties();
 
+function renderParties() {
+  const partyCards = state.parties.map((party) => {
+    const li = document.createElement("li");
+    li.innerHTML = 
+    `<h2>${party.name}</h2>
+    <p>${party.date}</p>
+    <p>${party.location}</p>
+    <p>${party.description}</p>`;
+    return li;
+  });
+  partyList.replaceChildren(...partyCards);
+}
